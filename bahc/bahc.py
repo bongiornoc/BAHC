eb5a4e68d5b7c94e9b852b03dedd678af9d2f126
Christian Bongiorno <christian.bongiorno@centralesupelec.fr>
2024-11-11 09:39:45 +0100

    Enhance BAHC class with random seed support and refactor methods for clarity

--- bahc/bahc.py
+++ bahc/bahc.py
@@ -3,9 +3,10 @@ from scipy.cluster.hierarchy import average, cophenet
 from scipy.spatial.distance import squareform
 from statsmodels.stats.correlation_tools import cov_nearest
 import random
+import timeit
 
 class BAHC:
-    def __init__(self, data, K=1, Nboot=100, method='near', filter_type='covariance'):
+    def __init__(self, data, K=1, Nboot=100, method='near', filter_type='covariance', seed=None):
         """
         Initializes the BAHC class and performs the filtering.
 
@@ -17,6 +18,7 @@ class BAHC:
         Nboot (int): Number of bootstraps.
         method (str): Regularization method ('no-neg' or 'near').
         filter_type (str): Type of filtering ('correlation' or 'covariance').
+        seed (int): Random seed.
 
         Notes:
         - For the basic BAHC method, see:
@@ -32,12 +34,33 @@ class BAHC:
         self.Nboot = Nboot
         self.method = method
         self.filter_type = filter_type
-        self.regularization_methods = {'no-neg': self.remove_negative_eigenvalues, 'near': cov_nearest}
+        self.regularization_methods = {'no-neg': self._remove_negative_eigenvalues, 'near': cov_nearest}
         self.N, self.T = data.shape
         self.std_devs = data.std(axis=1)
-        return self.filter_matrix()
+        self.rng = np.random.default_rng(seed)
+        self.indices = np.triu_indices(self.N, 1)
+        self.observations =  list(range(self.T))
+    
 
-    def hierarchical_cleaning(self, R):
+    def _shift_dendrogram(self,dendrogram):
+        """
+        Adjusts the dendrogram to ensure all values in the third column are non-negative.
+        This function shifts the values in the third column of the dendrogram such that the minimum value becomes zero.
+        If the minimum value is already non-negative, no shift is applied.
+        Parameters:
+        dendrogram (numpy.ndarray): A 2D array representing the dendrogram, where the third column contains the values to be adjusted.
+        Returns:
+        tuple: A tuple containing the adjusted dendrogram and the offset value used for the shift.
+        """
+        
+        if dendrogram[:,2].min()<0:
+            offset_value = dendrogram[:,2].min()
+            dendrogram[:,2] = dendrogram[:,2] - offset_value
+        else:
+            offset_value = 0
+        return dendrogram-offset_value,offset_value
+
+    def _hierarchical_cleaning(self, R):
         """
         Filters the input matrix using hierarchical clustering.
 
@@ -47,25 +70,27 @@ class BAHC:
         Returns:
         ndarray: Filtered matrix.
         """
-        distance = R[np.triu_indices(R.shape[0], 1)]
+        distance = R[self.indices]
         dendrogram = average(distance)
-        return 1 - squareform(cophenet(dendrogram))
+        
+        dendrogram,offset_value = self._shift_dendrogram(dendrogram)
+        
+        return 1 - (squareform(cophenet(dendrogram)+offset_value))
 
-    def generate_noise(self, N, T, epsilon=1e-10):
+
+    def _generate_noise(self, epsilon=1e-10):
         """
         Generates noise for the given dimensions.
 
         Parameters:
-        N (int): Number of rows.
-        T (int): Number of columns.
         epsilon (float): Standard deviation of the noise.
 
         Returns:
         ndarray: Generated noise matrix.
         """
-        return np.random.normal(0, epsilon, size=(N, T))
+        return self.rng.normal(0, epsilon, size=(self.N, self.T))
 
-    def remove_negative_eigenvalues(self, matrix):
+    def _remove_negative_eigenvalues(self, matrix):
         """
         Removes negative eigenvalues from the matrix.
 
@@ -99,7 +124,7 @@ class BAHC:
         for _ in range(n_iter):
             y = matrix
             R = matrix - D
-            matrix = self.remove_negative_eigenvalues(R)
+            matrix = self._remove_negative_eigenvalues(R)
             D = matrix - R
             np.fill_diagonal(matrix, diag)
             
@@ -110,7 +135,7 @@ class BAHC:
             print("Convergence not achieved")
         return matrix
 
-    def higher_order_covariance(self, C, K):
+    def _higher_order_covariance(self, C, K):
         """
         Computes higher order covariance matrices.
 
@@ -121,10 +146,10 @@ class BAHC:
         Yields:
         ndarray: Higher order covariance matrices.
         """
-        Cf = np.identity(C.shape[0])
+        Cf = np.identity(self.N)
         for i in range(max(K)):
             res = C - Cf
-            res = self.hierarchical_cleaning(res)
+            res = self._hierarchical_cleaning(1-res)
             np.fill_diagonal(res, 0)
             Cf += res
             if i + 1 in K:
@@ -137,26 +162,25 @@ class BAHC:
         Returns:
         ndarray: Filtered matrix (N x N). If K is a list, returns a list of matrices.
         """
-        noise_matrix = self.generate_noise(self.N, self.T)
+        noise_matrix = self._generate_noise()
         C = np.zeros((len(self.K), self.N, self.N))
 
         if self.Nboot == 0:
             # Compute the correlation matrix
             Cb = np.corrcoef(self.data)
             # Apply regularization and higher order filtering
-            C = np.array(list(map(self.regularization_methods[self.method], np.array(list(self.higher_order_covariance(Cb, self.K))))))
+            C = np.array(list(map(self.regularization_methods[self.method], np.array(list(self._higher_order_covariance(Cb, self.K))))))
             if self.filter_type == 'covariance':
                 C *= np.outer(self.std_devs, self.std_devs)
             return C[0] if len(self.K) == 1 else C
 
         for _ in range(self.Nboot):
             # Generate bootstrap samples with noise
-            bootstrap_sample = self.data[:, random.choices(range(self.T), k=self.T)] + noise_matrix
+            bootstrap_sample = self.data[:, self.rng.choice(self.observations, size=self.T)] + noise_matrix
             # Compute the correlation matrix
             Cb = np.corrcoef(bootstrap_sample)
             # Apply regularization and higher order filtering
-            C += np.array(list(map(self.regularization_methods[self.method], np.array(list(self.higher_order_covariance(Cb, self.K))))))
-
+            C += np.array(list(map(self.regularization_methods[self.method], np.array(list(self._higher_order_covariance(Cb, self.K))))))
         if self.filter_type == 'covariance':
             C = (C / self.Nboot) * np.outer(self.std_devs, self.std_devs)
         else:
@@ -166,8 +190,12 @@ class BAHC:
 
 if __name__ == '__main__':
     # Generate random data
-    x = np.random.normal(0, 1, size=(10, 100))
+    # set a seed
+    np.random.seed(42)
+    x = np.random.normal(0, 1, size=(100, 10))
     # Create an instance of CovarianceFilter and filter the data
-    filter_instance = BAHC(x, K=[1,2,3], Nboot=100, method='near', filter_type='correlation')
+    
+    bahc_instance = BAHC(x,K=[2,3], filter_type='correlation',seed=42)
+    filtered_matrix = bahc_instance.filter_matrix()
     # Print the filtered matrix
-    print(filter_instance.filtered_matrix)
+    print(filtered_matrix)
